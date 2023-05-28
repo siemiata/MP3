285e458f88576830b788f8319bb5007550a45be5
Aleksander Siemiątkowski <s22201@pjwstk.edu.pl>
2023-05-28 11:07:41 +0200

    wieloaspektowe

--- Person.java
+++ Person.java
@@ -1,14 +1,27 @@
 import java.util.EnumSet;
+import java.util.Optional;
 
 enum PersonType {Person, Owner, Trainer};
 public abstract class Person {
     private String name;
     private String surname;
+    private String gender;
+    private Optional<String> maidenName;
+    private Optional<String> militaryService;
     private EnumSet<PersonType> persons =  EnumSet.of(PersonType.Person);
 
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public Person(String name, String surname, String gender, Optional<String> maidenName, Optional<String> militaryService, EnumSet<PersonType> persons) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.maidenName = maidenName;
+        this.militaryService = militaryService;
+        this.persons = persons;
     }
 
     public abstract String hobby();
